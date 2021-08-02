f04bdd52c1157e0662467454d80ed25dcfbb6baf
Mohamed Medhat <mohamedmmedhat21@gmail.com>
2021-08-02 23:54:45 +0200

    adding App icp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBox from "./SearchBox";
 
-let done=true;
+let done = true;
 
 class App extends React.Component {
     constructor() {
@@ -14,26 +14,24 @@ class App extends React.Component {
     searchChange = (event) => {
         this.setState({ searchField: event.target.value });
     }
-    solve=()=>{
-        fetch(`http://numbersapi.com/${this.state.searchField}`).then(res => res.text()).then(data=>this.setState({parag:data}));
+    solve = () => {
+        fetch(`http://numbersapi.com/${this.state.searchField}`).then(res => res.text()).then(data => this.setState({ parag: data }));
         done = /^\d+$/.test(this.state.searchField);
     }
-    componentDidMount(){
-        document.title = "Numbers Facts"
-      }
+
     render() {
-        if(done===true){
-        return (
-            <div className='tc'>
-                <h1 style={{ fontSize: 80 }}>Numbers Facts</h1>
-                <h3 style={{ fontWeight: 600 }}>Interesting facts about numbers!</h3>
-                <p style={{ fontWeight: 400, fontSize: 25 }}>Enter a random number and it will display a fact related to that number.</p>
-                <SearchBox searchChange={this.searchChange} solve={this.solve} />
-                <h3>{this.state.parag}</h3>
-            </div>
-        );
+        if (done === true) {
+            return (
+                <div className='tc'>
+                    <h1 style={{ fontSize: 80 }}>Numbers Facts</h1>
+                    <h3 style={{ fontWeight: 600 }}>Interesting facts about numbers!</h3>
+                    <p style={{ fontWeight: 400, fontSize: 25 }}>Enter a random number and it will display a fact related to that number.</p>
+                    <SearchBox searchChange={this.searchChange} solve={this.solve} />
+                    <h3>{this.state.parag}</h3>
+                </div>
+            );
         }
-        else{
+        else {
             return (
                 <div className='tc'>
                     <h1 style={{ fontSize: 80 }}>Numbers Facts</h1>
